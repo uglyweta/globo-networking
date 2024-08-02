81fb0b0334d191c597959f5d1cc6b3c942efae75
Phil Cuff <phil.cuff@aderant.com>
2024-08-02 15:35:35 +1200

    fixed workspace tag and make variables required

--- resources.tf
+++ resources.tf
@@ -19,7 +19,7 @@ locals {
   common_tags = {
     Environment = var.environment
     BillingCode = var.billing_code
-    Workspace   = Terraform.Workspace
+    Workspace   = terraform.workspace
   }
 }
 
