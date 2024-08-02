58246df696e241b2eaa3453848273cf77093a7d6
Phil Cuff <phil.cuff@aderant.com>
2024-08-02 14:48:18 +1200

    re ran cloud CloudFormation dep-net deployment

--- imports.tf
+++ imports.tf
@@ -4,45 +4,45 @@
 
 import {
   to = module.main.aws_vpc.this[0]
-  id = "vpc-0daef8c066e7a3d3e" #VPC
+  id = "vpc-0bba03e71985269d6" #VPC
 }
 
 import {
   to = module.main.aws_subnet.public[0]
-  id = "subnet-034a7bd577ac7c9d4" #PublicSubnet1
+  id = "subnet-08f23eb825711cdb6" #PublicSubnet1
 }
 
 import {
   to = module.main.aws_subnet.public[1]
-  id = "subnet-0adcd0eda54e2d615" #PublicSubnet2
+  id = "subnet-0cd7adcef79a77c09" #PublicSubnet2
 }
 
 import {
   to = module.main.aws_internet_gateway.this[0]
-  id = "igw-017934cfa15939d78" #InternetGateway
+  id = "igw-0440cfb8270079998" #InternetGateway
 }
 
 import {
   to = module.main.aws_route.public_internet_gateway[0]
-  id = "rtb-0761831d5415c3223_0.0.0.0/0" #DefaultPublicRoute
+  id = "rtb-097834b9113561c7f_0.0.0.0/0" #DefaultPublicRoute
 }
 
 import {
   to = module.main.aws_route_table.public[0]
-  id = "rtb-0761831d5415c3223" #PublicRouteTable
+  id = "rtb-097834b9113561c7f" #PublicRouteTable
 }
 
 import {
   to = module.main.aws_route_table_association.public[0]
-  id = "subnet-034a7bd577ac7c9d4/rtb-0761831d5415c3223" #PublicSubnet1/PublicRouteTable
+  id = "subnet-08f23eb825711cdb6/rtb-097834b9113561c7f" #PublicSubnet1/PublicRouteTable
 }
 
 import {
   to = module.main.aws_route_table_association.public[1]
-  id = "subnet-0adcd0eda54e2d615/rtb-0761831d5415c3223" #PublicSubnet2/PublicRouteTable
+  id = "subnet-0cd7adcef79a77c09/rtb-097834b9113561c7f" #PublicSubnet2/PublicRouteTable
 }
 
 import {
   to = aws_security_group.ingress
-  id = "sg-058cdff219e58aa06" #NoIngressSecurityGroup
+  id = "sg-029d7919fbff8c693" #NoIngressSecurityGroup
 }
