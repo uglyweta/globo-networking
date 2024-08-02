b9cd72a3422354071643125f69b9459e4817580f
Phil Cuff <phil.cuff@aderant.com>
2024-08-02 15:32:56 +1200

    Add workspace tag and make variables required

--- variables.tf
+++ variables.tf
@@ -11,13 +11,13 @@ variable "region" {
 variable "prefix" {
   type        = string
   description = "(Optional) Prefix to use for all resources in this module. Default: globo-dev"
-  
+
 }
 
 variable "environment" {
   type        = string
   description = "(Optional) Environment of all resources)"
-  
+
 }
 
 variable "billing_code" {
@@ -30,7 +30,7 @@ variable "billing_code" {
 variable "cidr_block" {
   type        = string
   description = "(Optional) The CIDR block for the VPC. Default:10.42.0.0/16"
-  
+
 }
 
 variable "public_subnets" {
